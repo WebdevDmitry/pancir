6daf8a1b5dd7ab6711ee59fe7a81c6260790ebc4
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-07-03 22:04:47 +0300

    add geolocation

--- scripts/js.js
+++ scripts/js.js
@@ -4,3 +4,23 @@ $(document).ready(function() {
     });
 });
 
+
+            // YANDEX MAPS
+
+
+ymaps.ready(init);
+function init(){ 
+    var myMap = new ymaps.Map("map", {
+        center: [55.707618, 37.697415],
+        zoom: 16
+    }); 
+    
+    var myPlacemark = new ymaps.Placemark([55.707618, 37.697415], {
+        hintContent: 'ул. Южнопортовая, дом 9',
+        balloonContent: 'Панцирь'
+    });
+    
+    myMap.geoObjects.add(myPlacemark);
+}
+
+
